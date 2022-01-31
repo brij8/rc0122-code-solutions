3d5a03351a872f8e890baffda6d1f413846d4175
Brighitte Johnson <bmuses@gmail.com>
2022-01-31 10:47:58 -0800

    completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+var fullName = 'brij';
+var isCool = true;
+var totalPets = 2;
+var unknown = null;
+var never;
+
+console.log('value of fullname: ', fullName);
+console.log('value of IsCool: ', isCool);
+console.log('value of totalPets: ', totalPets);
+console.log('typeof fullName: ', typeof fullName);
+console.log('typeof isCool: ', typeof isCool);
+console.log('typeof totalPets: ', typeof totalPets);
+console.log('value of unknown: ', unknown);
+console.log('typeof unknown: ', typeof unknown);
+console.log('value of never: ', never);
+console.log('typeof never: ', typeof never);
