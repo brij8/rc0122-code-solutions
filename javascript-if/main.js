5be405c0f3ddf908880f95ee150e8e4e61b52f6d
Brighitte Johnson <bmuses@gmail.com>
2022-02-02 13:49:00 -0800

    completed javascript-if

---
+++ javascript-if/main.js
@@ -0,0 +1,61 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+function isUnderFive(num) {
+  return num < 5;
+}
+
+function isEven(num) {
+  return (num % 2 === 0);
+}
+
+function startsWithJ(string) {
+  return string[0] === 'J' || string[0] === 'j';
+}
+
+function isOldEnoughToDrink(person) {
+  return person.age >= 21;
+}
+function isOldEnoughToDrive(person) {
+  return person.age >= 16;
+}
+
+function isOldEnoughToDrinkAndDrive(person) {
+  return 'false - this is illegal';
+}
+
+function categorizeAcidity(pH) {
+  if (pH > 0 && pH < 7) {
+    return 'acid';
+  } else {
+    if (pH === 7) {
+      return 'neutral';
+    } else {
+      if (pH > 7 && pH < 14) {
+        return 'base';
+      } else {
+        if (pH < 0 || pH > 14) {
+          return 'invalid pH level';
+        }
+      }
+    }
+  }
+}
+
+function introduceWarnerBro(name) {
+  if (name === 'wakko' || name === 'yakko') {
+    return "We're the Warner Brothers!";
+  } else {
+    if (name === 'dot') {
+      return "I'm cute~";
+    } else {
+      return 'Goodnight everybody!';
+    }
+  }
+}
