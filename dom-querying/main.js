a218e505f1a4de2b9319134ce7b7188219629d3e
Brighitte Johnson <bmuses@gmail.com>
2022-02-07 11:53:12 -0800

    dom-querying but with $variable assignments

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,9 +1,28 @@
 console.log('hello, world');
-console.log('h1: ', document.querySelector('h1'));
-console.dir(document.querySelector('h1'));
-console.log('#explanation: ', document.querySelector('#explanation'));
+
+var $heading = document.querySelector('h1');
+console.log('h1: ', $heading);
+
+var $explanation = document.querySelector('#explanation');
+console.log('#explanation: ', $explanation);
 console.dir(document.querySelector('#explanation'));
-console.log('.hint: ', document.querySelector('.hint'));
+
+var $hint = document.querySelector('.hint');
+console.log('.hint: ', $hint);
 console.dir(document.querySelector('.hint'));
-console.log('p node list: ', document.querySelectorAll('p'));
-console.log('.example-link node list', document.querySelectorAll('.example-link'));
+
+var $p = document.querySelectorAll('p');
+console.log('p node list: ', $p);
+
+var $examplelink = document.querySelectorAll('.example-link');
+console.log('.example-link node list', $examplelink);
+
+// console.log('hello, world');
+// console.log('h1: ', document.querySelector('h1'));
+// console.dir(document.querySelector('h1'));
+// console.log('#explanation: ', document.querySelector('#explanation'));
+// console.dir(document.querySelector('#explanation'));
+// console.log('.hint: ', document.querySelector('.hint'));
+// console.dir(document.querySelector('.hint'));
+// console.log('p node list: ', document.querySelectorAll('p'));
+// console.log('.example-link node list', document.querySelectorAll('.example-link'));
