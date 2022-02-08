ca935831682b8350070aee35c336f8d1f4b69528
Brighitte Johnson <bmuses@gmail.com>
2022-02-07 17:29:35 -0800

    dom-manipulation improvements

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -1,31 +1,53 @@
-var $clicks = 0;
-var $hotbutt = document.querySelector('.hot-button');
+var clicks = 0;
+var $hotbutton = document.querySelector('.hot-button');
 var $clickcount = document.querySelector('.click-count');
 
-$hotbutt.addEventListener('click', function (event) {
-  $clicks++;
-  $clickcount.textContent = $clicks;
-  if ($clicks < 4) {
-    $hotbutt.className = 'hot-button cold';
+$hotbutton.addEventListener('click', handleHotButtonClick);
+
+function handleHotButtonClick(event) {
+  clicks++;
+  var temp;
+  if (clicks < 4) {
+    temp = 'cold';
+  } else if (clicks < 7) {
+    temp = 'cool';
+  } else if (clicks < 10) {
+    temp = 'tepid';
+  } else if (clicks < 13) {
+    temp = 'warm';
+  } else if (clicks < 16) {
+    temp = 'hot';
   } else {
-    if ($clicks < 7) {
-      $hotbutt.className = 'hot-button cool';
-    } else {
-      if ($clicks < 10) {
-        $hotbutt.className = 'hot-button tepid';
-      } else {
-        if ($clicks < 13) {
-          $hotbutt.className = 'hot-button warm';
-        } else {
-          if ($clicks < 16) {
-            $hotbutt.className = 'hot-button hot';
-          } else {
-            if ($clicks >= 17) {
-              $hotbutt.className = 'hot-button nuclear';
-            }
-          }
-        }
-      }
-    }
+    temp = 'nuclear';
   }
-});
+  $hotbutton.className = 'hot-button ' + temp;
+  $clickcount.textContent = 'Clicks: ' + clicks;
+}
+
+// $hotbutt.addEventListener('click', function (event) {
+//   $clicks++;
+//   $clickcount.textContent = $clicks;
+//   if ($clicks < 4) {
+//     $hotbutt.className = 'hot-button cold';
+//   } else {
+//     if ($clicks < 7) {
+//       $hotbutt.className = 'hot-button cool';
+//     } else {
+//       if ($clicks < 10) {
+//         $hotbutt.className = 'hot-button tepid';
+//       } else {
+//         if ($clicks < 13) {
+//           $hotbutt.className = 'hot-button warm';
+//         } else {
+//           if ($clicks < 16) {
+//             $hotbutt.className = 'hot-button hot';
+//           } else {
+//             if ($clicks >= 17) {
+//               $hotbutt.className = 'hot-button nuclear';
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// });
