00506a04bd98ca4c62cea4bd65baef91f3db5f96
brij <bmuses@gmail.com>
2022-02-22 15:40:58 -0800

    completed javascript-timers

---
+++ javascript-timers/interval.js
@@ -0,0 +1,16 @@
+setInterval(bowie, 1000);
+
+function bowie() {
+  var h1msg = document.querySelector('.countdown-display');
+  if (h1msg.textContent === '4') {
+    h1msg.textContent = '3';
+  } else if (h1msg.textContent === '3') {
+    h1msg.textContent = '2';
+  } else if
+  (h1msg.textContent === '2') {
+    h1msg.textContent = '1';
+  } else if
+  (h1msg.textContent === '1') {
+    h1msg.textContent = '~Earth Beeeelooowww Us~';
+  }
+}
