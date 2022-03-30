b25fa0f58cd773bad452c932a0b9c288705fdd1e
brij <bmuses@gmail.com>
2022-03-30 16:00:13 -0700

    react-function-components + notes

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -7,3 +7,9 @@ function CustomButton(props) {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(CustomButton());
+
+// this also works
+// root.render(<CustomButton />);
+
+// React 17 version:
+// ReactDOM.render(<CustomButton />, document.querySelector('#root'));
