cd524dd522ed2c92215d79caa72591ac4a781fe0
brij <bmuses@gmail.com>
2022-03-31 11:47:09 -0700

    completed react-events-and-state

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isClicked: !prevState.isClicked
+    }));
+  }
+
+  render() {
+    return (
+    <button onClick={this.handleClick}>
+        {this.state.isClicked ? 'un-Clicky' : 'Clicky'}
+    </button>);
+  }
+}
+
+ReactDOM.render(
+  <CustomButton />,
+  document.querySelector('#root')
+);
+
+// in React 18
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<CustomButton />);
