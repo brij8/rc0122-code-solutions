5a9167c55e61cd50a4684cb5e7d4789afa89e835
brij <bmuses@gmail.com>
2022-04-01 15:38:02 -0700

    react-hot-button-component + state fix

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -5,31 +5,33 @@ class HotButton extends React.Component {
     super(props);
     this.handleClick = this.handleClick.bind(this);
     this.state = {
-      clicks: 0,
-      className: 'hot-button cold'
+      clicks: 0
     };
   }
 
   handleClick() {
     this.setState({ clicks: this.state.clicks + 1 });
+  }
+
+  setClass() {
     if (this.state.clicks < 4) {
-      this.setState({ className: 'hot-button cold' });
+      return 'hot-button cold';
     } else if (this.state.clicks < 7) {
-      this.setState({ className: 'hot-button cool' });
+      return 'hot-button cool';
     } else if (this.state.clicks < 10) {
-      this.setState({ className: 'hot-button tepid' });
+      return 'hot-button tepid';
     } else if (this.state.clicks < 13) {
-      this.setState({ className: 'hot-button warm' });
+      return 'hot-button warm';
     } else if (this.state.clicks < 16) {
-      this.setState({ className: 'hot-button hot' });
+      return 'hot-button hot';
     } else {
-      this.setState({ className: 'hot-button nuclear' });
+      return 'hot-button nuclear';
     }
   }
 
   render() {
     return (
-      <button className={this.state.className} onClick={this.handleClick}>
+      <button className={this.setClass()} onClick={this.handleClick}>
         Hot Button
       </button>);
   }
