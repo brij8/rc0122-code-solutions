b9aa0b086963a7996ca19a816b7a65558a7cb2a7
brij <bmuses@gmail.com>
2022-04-03 16:59:22 -0700

    completed react-stopwatch-component

---
+++ react-stopwatch-component/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import StopWatch from './stopwatch';
+
+ReactDOM.render(
+  <div>
+    <StopWatch />
+  </div>,
+  document.querySelector('#root')
+);
