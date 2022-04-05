cec483039772766e941c1d83056b95b5ffffbff6
brij <bmuses@gmail.com>
2022-04-05 15:13:06 -0700

    express-static + small change

--- express-static/index.js
+++ express-static/index.js
@@ -1,4 +1,4 @@
-// In index.js, require() both express and Node's path module.
+// In index.js, require() both express and Node's path module:
 const express = require('express');
 const path = require('path');
 
@@ -12,7 +12,9 @@ const absolutePath = path.join('__dirname', 'public');
 console.log('absolutePath: ', absolutePath);
 
 // Use express.static() to create a middleware function that will automatically serve any files located in your public directory. Put the return value into a variable.
-const pubDir = express.static('public');
+
+// const pubDir = express.static('public');
+const pubDir = express.static(absolutePath);
 
 // Mount the middleware you just created with the app.use()
 app.use(pubDir);
