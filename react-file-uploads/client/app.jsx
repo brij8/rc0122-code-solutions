4e88d991334f083bc9e8e1763436bcc37fbd987d
brij <bmuses@gmail.com>
2022-04-11 10:45:50 -0700

    completed react-file-uploads + notes

--- react-file-uploads/client/app.jsx
+++ react-file-uploads/client/app.jsx
@@ -24,8 +24,8 @@ export default class App extends React.Component {
     // * 1. "caption" the value of this.state.caption
     // * 2. "image" the value of this.fileInputRef.current.files[0]
     const formData = new FormData();
-    formData.append('caption', 'this.state.caption');
-    formData.append('image', 'this.fileInputRef.current.files[0]');
+    formData.append('caption', this.state.caption);
+    formData.append('image', this.fileInputRef.current.files[0]);
     // * Use fetch() to send a POST request to /api/uploads. The body
     // * should be the form data object you created(not a JSON string)
     // * Headers are not necessary as fetch will use the correct Content - Type
@@ -38,11 +38,13 @@ export default class App extends React.Component {
     //* parse the JSON response body
       .then(response => response.json())
     //*   log the parsed response body
-      .then(data => console.log('response.json: ', data));
-    //*   set the caption state back to an empty string
-    this.setState({ caption: '' });
-    //*   assign null to this.fileInputRef.current.value to clear the file
-    this.fileInputRef.current.value = null;
+      .then(data => {
+        console.log('response.json: ', data);
+        //*   set the caption state back to an empty string
+        this.setState({ caption: '' });
+        //*   assign null to this.fileInputRef.current.value to clear the file
+        this.fileInputRef.current.value = null;
+      });
     // * Catch any error in the promise chain.
     req.catch(err => {
       console.err(err);
